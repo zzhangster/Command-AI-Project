6bf58dc73fcb5af00c2bfbfbb09a9bdce6a50a79
Zhiheng Zhang <zhihengzhang2@gmail.com>
2017-12-03 18:13:10 -0600

    Edits

--- ThreatRangeLibrary.lua
+++ ThreatRangeLibrary.lua
@@ -6,48 +6,18 @@ ScenEdit_SetKeyValue("thr_2478","80")
 ScenEdit_SetKeyValue("thr_1276","80")
 -- KN-06
 ScenEdit_SetKeyValue("thr_3056","80")
--- SA-10 Family 
-ScenEdit_SetKeyValue("thr_393","40")
-ScenEdit_SetKeyValue("thr_2914","40")
-ScenEdit_SetKeyValue("thr_393","40")
-ScenEdit_SetKeyValue("thr_393","40")
-ScenEdit_SetKeyValue("thr_393","40")
-ScenEdit_SetKeyValue("thr_393","40")
-ScenEdit_SetKeyValue("thr_393","40")
-
-
-
-ScenEdit_SetKeyValue("thr_760","20")
-ScenEdit_SetKeyValue("thr_761","20")
-ScenEdit_SetKeyValue("thr_907","20")
-ScenEdit_SetKeyValue("thr_762","20")
-ScenEdit_SetKeyValue("thr_762","20")
-
-
-
-
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
--- All Ship SAM Threat Ranges (NM)
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
-ScenEdit_SetKeyValue("thr_130","110")
+-- SA-20b Family
+ScenEdit_SetKeyValue("thr_2027","110")
+ScenEdit_SetKeyValue("thr_386","110")
+ScenEdit_SetKeyValue("thr_3013","110")
+-- SA-21a
+ScenEdit_SetKeyValue("thr_543","140")
+ScenEdit_SetKeyValue("thr_1668","140")
+ScenEdit_SetKeyValue("thr_3013","140")
+ScenEdit_SetKeyValue("thr_543","140")
+ScenEdit_SetKeyValue("thr_2442","220")
+ScenEdit_SetKeyValue("thr_2029","220")
+ScenEdit_SetKeyValue("thr_1937","220")
+-- SA-23
+ScenEdit_SetKeyValue("thr_2277","120")
+ScenEdit_SetKeyValue("thr_1860","120")
